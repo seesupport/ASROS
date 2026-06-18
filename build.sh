e41878146ebec70296ccf0783071b72f55a62771
seesupport <127085082+seesupport@users.noreply.github.com>
2026-06-18 20:44:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Build the UEFI application
 TINYGO=${TINYGO:-tinygo}
+# Generate ISR stubs
+python3 tools/gen_isr.py
+# Then build
 $TINYGO build -o rtos.efi -target=x86_64-unknown-uefi -no-debug ./boot/uefi
